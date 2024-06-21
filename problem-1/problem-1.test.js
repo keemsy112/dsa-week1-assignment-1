85038eba46582d8f2261cda85b6eaf2f8fddb844
shinyoungKim <keemsy112@gmail.com>
2024-06-22 00:35:06 +0900

    initial commit

--- problem-1/problem-1.test.js
+++ problem-1/problem-1.test.js
@@ -1,18 +1,30 @@
 const solution = (numbers) => {
+  let sum = 0;
+
+  while (true) {
+    if (numbers.length === 0) {
+      return sum;
+    }
+
+    const [first] = numbers;
+    sum = sum + first;
+    numbers = numbers.slice(1);
+  }
 };
 
-test('빈 배열은 0을 반환한다', () => {
+test("빈 배열은 0을 반환한다", () => {
   expect(solution([])).toBe(0);
 });
 
-test('배열의 합을 반환한다', () => {
+test("배열의 합을 반환한다", () => {
   expect(solution([1, 2, 3, 4])).toBe(10);
   expect(solution([-1, 3, 8, 9, 10, 11])).toBe(40);
 });
 
-test('큰 배열이 입력으로 주어져도 RangeError를 던지지 않는다', () => {
+test("큰 배열이 입력으로 주어져도 RangeError를 던지지 않는다", () => {
   const input = Array.from({ length: 10000 }, (_, i) => i + 1);
 
-  expect(() => solution(input))
-    .not.toThrowError(new RangeError('Maximum call stack size exceeded'));
+  expect(() => solution(input)).not.toThrowError(
+    new RangeError("Maximum call stack size exceeded")
+  );
 });
